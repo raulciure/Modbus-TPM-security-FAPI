2ea0f482b98838f8695d96a504cd8bddffa9d79e
Raul Ciure <raulciure@yahoo.com>
2026-06-03 13:23:30 +0300

    Update security.py
    Improved DH KDF logic by replacing plain SHA256 with HKDF key derivation, with a hardcoded 32-byte random salt.

--- src/modbus_tpm_security_fapi/security.py
+++ src/modbus_tpm_security_fapi/security.py
@@ -3,6 +3,7 @@ from Crypto.Signature import pss
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import ECC
 from Crypto.Protocol import DH
+from Crypto.Protocol.KDF import HKDF
 
 
 def verify_RSA_signature(RSA_key : bytes | str, data : bytes, signature : bytes) -> bool:
@@ -18,7 +19,7 @@ def verify_RSA_signature(RSA_key : bytes | str, data : bytes, signature : bytes)
 
 # Generate an ECC key
 def ECC_key_gen() -> ECC.EccKey:
-    ECC_CURVE = "Curve25519"    # X25519 curve
+    ECC_CURVE = "Curve25519"    # X25519 key exchange protocol
     key = ECC.generate(curve=ECC_CURVE)    # type: ignore
     return key
 
@@ -37,8 +38,11 @@ def ECC_public_key_import(encoded_key : bytes) -> ECC.EccKey:
 # Create a common key based on both parties keys
 def ECDHE_key_agreement(own_key : ECC.EccKey, peer_key : ECC.EccKey) -> bytes:
     def kdf(input):
-        return SHA256.new(input)
+        SALT = bytes.fromhex("aa70fb5153d697261376069b636b377f1ee0ec03fdaf452f674caa3500ce47e7")
+        return HKDF(input, key_len=32, salt=SALT, hashmod=SHA256)
 
     session_key = DH.key_agreement(eph_priv=own_key, eph_pub=peer_key, kdf=kdf)
 
-    return session_key.digest()
+    if isinstance(session_key, tuple):
+        raise AssertionError("[ECDHE_key_agreement] session_key is a tuple (i.e. KDF created multiple keys)")
+    return session_key
